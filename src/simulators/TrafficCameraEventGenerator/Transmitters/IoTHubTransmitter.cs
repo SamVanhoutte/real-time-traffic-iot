48d5dc9119ed9b471cb61300ef47f7494d975380
Sam Vanhoutte <Sam.Vanhoutte@codit.eu>
2019-01-09 21:54:12 +0100

    resilient iot hub transmitter with polly

--- src/simulators/TrafficCameraEventGenerator/Transmitters/IoTHubTransmitter.cs
+++ src/simulators/TrafficCameraEventGenerator/Transmitters/IoTHubTransmitter.cs
@@ -4,12 +4,16 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Client.Exceptions;
+using NLog;
+using Polly;
 using TrafficCameraEventGenerator.Cars;
 
 namespace TrafficCameraEventGenerator.Transmitters
 {
     public class IoTHubTransmitter : IEventTransmitter
     {
+        private readonly ILogger _logger = LogManager.GetCurrentClassLogger();
         private readonly string _deviceConnectionString;
         private DeviceClient _deviceClient;
 
@@ -26,7 +30,28 @@ namespace TrafficCameraEventGenerator.Transmitters
 
         public async Task Transmit(CameraEvent cameraEvent, CancellationToken cancellationToken)
         {
-            await IoTHubClient.SendEventAsync(new Message(Encoding.UTF8.GetBytes(cameraEvent.ToJson())), cancellationToken);
+            try
+            {
+                var policy = Policy
+                    .Handle<TimeoutException>()
+                    .Or<DeviceMessageLockLostException>()
+                    .Or<IotHubCommunicationException>()
+                    .Or<IotHubThrottledException>()
+                    .Or<ServerBusyException>()
+                    .WaitAndRetryAsync(5, retryAttempt =>
+                        TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                        (exception, span, retryCount, context) => { _logger.Warn(exception, $"Retry {retryCount}/{5} sending to IoT Hub, because of exception {exception}"); });
+
+                // Retry maximum 5 times with exponential backoff for the above exceptions
+                await policy.ExecuteAsync(async (token) =>
+                {
+                    await IoTHubClient.SendEventAsync(new Message(Encoding.UTF8.GetBytes(cameraEvent.ToJson())), token);
+                }, cancellationToken);
+            }
+            catch (Exception e)
+            {
+                _logger.Error(e, $"An error occurred when trying to send message to IoT Hub: {e.Message}");
+            }
         }
     }
 }
