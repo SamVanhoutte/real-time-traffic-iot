cdf87e2ba8ee5ee9804fce660add806ba008e88a
Sam Vanhoutte <Sam.Vanhoutte@codit.eu>
2019-01-09 10:17:30 +0100

    initial project with docker support

---
+++ src/simulators/TrafficCameraService/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TrafficCameraService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Hello World!");
+                Task.Delay(5000).Wait();
+            }
+        }
+    }
+}
